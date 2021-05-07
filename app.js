33721c89536edd3f339228bd98bc39827317f8a7
justeSi <69959214+justeSi@users.noreply.github.com>
2021-05-07 10:05:19 +0300

    *pertvarka

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ const navbarLinks = document.querySelectorAll('.navbar-links')[0]
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+var dt = new Date();
+
+year  = dt.getFullYear();
+month = (dt.getMonth() + 1).toString().padStart(2, "0"); //padStart dėl dviženklio skaičiaus//
+day   = dt.getDate().toString().padStart(2, "0");
+data = day + '.' + month + '.' + year;
+document.getElementById("data").innerHTML = data;
